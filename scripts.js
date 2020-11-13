e9ab6e826688127ab7880a4cb36289e4a6a5fb92
vmkul <55065764+vmkul@users.noreply.github.com>
2020-11-14 00:18:43 +0200

    Add product info template

--- scripts.js
+++ scripts.js
@@ -4,6 +4,8 @@ const menuButton = document.getElementById('menu-button');
 const mobileMenu = document.getElementById('mobile-menu-layer');
 const pageContent = document.getElementById('page-content');
 
+const serverAddress = 'http://localhost:4000';
+
 menuButton.onclick = () => {
   if (mobileMenu.style.display === 'block') {
     mobileMenu.style.display = 'none';
@@ -13,7 +15,7 @@ menuButton.onclick = () => {
 };
 
 const productTemplate =
-  '  <div class="prod-img" onclick="window.location.hash = \'#product/{{ url }}\'">\n' +
+  '  <div class="prod-card-img" onclick="window.location.hash = \'#product/{{ url }}\'">\n' +
   '    <div class="btn-favorite " data-auth="0">\n' +
   '      <i class="fa  fa-heart-o " aria-hidden="true"></i>\n' +
   '    </div>\n' +
@@ -78,6 +80,72 @@ const productTemplate =
   '    To order\n' +
   '  </div>\n';
 
+const productInfoTemplate =
+  '  <div class="prod-info-img" onclick="window.location.hash = \'#product/{{ url }}\'">\n' +
+  '    <div class="btn-favorite " data-auth="0">\n' +
+  '      <i class="fa  fa-heart-o " aria-hidden="true"></i>\n' +
+  '    </div>\n' +
+  '    <img src="{{ image }}" alt="fish">\n' +
+  '\n' +
+  '    <div class="bg-buy">\n' +
+  '      <div class="btn-delete-buy"></div>\n' +
+  '      <div class="delete-name">Remove from cart</div>\n' +
+  '    </div>\n' +
+  '  </div>\n' +
+  '\n' +
+  '  <div><div class="name-prod">\n' +
+  '    <a href="#product/{{ url }}">\n' +
+  '      {{ productName }}\n' +
+  '    </a>\n' +
+  '    <meta content="">\n' +
+  '  </div>\n' +
+  '  <div class="description" style="position: relative">\n' +
+  '  </div>\n' +
+  '  <div class="main-block-attr-price">\n' +
+  '\n' +
+  '    <div class="block-table">\n' +
+  '      <table class="table">\n' +
+  '        <tbody>\n' +
+  '        <tr class="attr-img">\n' +
+  '\n' +
+  '          <th><span class="weight"></span></th>\n' +
+  '\n' +
+  '          <th><span class="quantity"></span></th>\n' +
+  '\n' +
+  '          <th><span class="Kcal"></span></th>\n' +
+  '\n' +
+  '        </tr>\n' +
+  '        <tr class="attr-number">\n' +
+  '\n' +
+  '          <th>{{ weight }}</th>\n' +
+  '\n' +
+  '          <th>{{ quantity }}</th>\n' +
+  '\n' +
+  '          <th>{{ KCal }}</th>\n' +
+  '\n' +
+  '        </tr>\n' +
+  '        </tbody>\n' +
+  '      </table>\n' +
+  '    </div>\n' +
+  '\n' +
+  '    <div class="block-price-buy">\n' +
+  '      <div class="block-price">\n' +
+  '        <div class="price"><span>{{ price }}</span> <span>грн</span></div>\n' +
+  '\n' +
+  '      </div>\n' +
+  '      <div class="category-count">\n' +
+  '        <div class="count-inner">\n' +
+  '          <span class="cat-minus">‒</span>\n' +
+  '          <input type="text" class="prod-count" value="1">\n' +
+  '          <span class="cat-plus">+</span>\n' +
+  '        </div>\n' +
+  '      </div>\n' +
+  '    </div>\n' +
+  '  </div>\n' +
+  '  <div class="btn-buy">\n' +
+  '    To order\n' +
+  '  </div></div>\n';
+
 const emptyDiv = element => {
   while (element.firstChild) {
     pageContent.removeChild(element.firstChild);
@@ -91,9 +159,9 @@ window.addEventListener('popstate', async () => {
   loader.alt = 'loader';
   pageContent.appendChild(loader);
   const hash = document.location.hash;
-  console.log(hash);
+  window.scroll(0, 0);
   if (hash === '') {
-    const response = await fetch('http://localhost:4000/products');
+    const response = await fetch(`${serverAddress}/products`);
     const parsed = await response.json();
     emptyDiv(pageContent);
     const productContainer = document.createElement('div');
@@ -107,10 +175,16 @@ window.addEventListener('popstate', async () => {
       product.innerHTML = Mustache.render(productTemplate, productInfo);
     });
   } else if (hash.startsWith('#product')) {
-    pageContent.innerHTML = `<h1>${hash}</h1>`;
-
+    const url = `${serverAddress}/product${hash.substring(hash.indexOf('/'), hash.length)}`;
+    const response = await fetch(url);
+    const parsed = await response.json();
+    emptyDiv(pageContent);
+    const product = document.createElement('div');
+    product.className = 'prod-info-block';
+    pageContent.appendChild(product);
+    product.innerHTML = Mustache.render(productInfoTemplate, parsed);
   } else {
-    pageContent.innerHTML = '<h1>Not found</h1>'
+    pageContent.innerHTML = '<h1>Not found</h1>';
   }
 });
 
