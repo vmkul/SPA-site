8e8ab551f37848d869b5e36d7332c71054b0ea41
vmkul <55065764+vmkul@users.noreply.github.com>
2020-12-16 03:06:16 +0200

    Map style modules

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
     collectCoverage: true,
     transform: { '\\.js$': 'babel-jest', },
+    moduleNameMapper: {
+        "\\.(css|sass)$": "<rootDir>/scripts/tests/mocks/style.sass.js"
+    }
 };
